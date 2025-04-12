829f42237b70c5d7971feb487897a22b277a4489
Kishor Bankar <linkekishor@gmail.com>
2025-04-12 17:42:14 +0530

    Refactor calculation logic into a separate function for better readability and maintainability

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,36 @@ import (
 	"strconv"
 )
 
+
+func calculatedByUser(firstArg, secondArg int) {
+    fmt.Println("Performing calculations...")
+    fmt.Printf("First Arg: %d\n", firstArg)
+    fmt.Printf("Second Arg: %d\n", secondArg)
+
+    fmt.Printf("Sum: %d\n", utils.Add(firstArg, secondArg))
+    fmt.Printf("Difference: %d\n", utils.Subtract(firstArg, secondArg))
+    fmt.Printf("Multi: %d\n", utils.Multiply(firstArg, secondArg))
+    quotient, err := utils.Divide(firstArg, secondArg)
+    if err != nil {
+        fmt.Println("Error:", err)
+    } else {
+        fmt.Printf("Quotient: %d\n", quotient)
+    }
+    fmt.Printf("First Arg Is Even: %t\n", utils.IsEven(firstArg))
+    fmt.Printf("First Arg Is Odd: %t\n", utils.IsOdd(firstArg))
+    fmt.Printf("Second Arg Is Even: %t\n", utils.IsEven(secondArg))
+    fmt.Printf("Second Arg Is Odd: %t\n", utils.IsOdd(secondArg))
+    fmt.Printf("Factorial: %d\n", utils.Factorial(firstArg))
+    fmt.Println("Calculations completed successfully.")
+}
+
 func main() {
 	fmt.Println("Welcome to My Go Project!")
 
 	if len(os.Args) < 3 {
 		fmt.Println("Usage: ./my-go-app <firstArg> <secondArg>")
 		return
-	}
+	} 
 
 	// Parse arguments
 	firstArg, err1 := strconv.Atoi(os.Args[1])
@@ -23,19 +46,8 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Sum: %d\n", utils.Add(firstArg, secondArg))
-	fmt.Printf("Difference: %d\n", utils.Subtract(firstArg, secondArg))
-	fmt.Printf("Multi: %d\n", utils.Multiply(firstArg, secondArg))
-	quotient, err := utils.Divide(firstArg, secondArg)
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Printf("Quotient: %d\n", quotient)
-	}
-	fmt.Printf("First Arg Is Even: %t\n", utils.IsEven(firstArg))
-	fmt.Printf("First Arg Is Odd: %t\n", utils.IsOdd(firstArg))
-	fmt.Printf("Second Arg Is Even: %t\n", utils.IsEven(secondArg))
-	fmt.Printf("Second Arg Is Odd: %t\n", utils.IsOdd(secondArg))
-	fmt.Printf("Factorial: %d\n", utils.Factorial(firstArg))
-	fmt.Println("Goodbye!")
+    // Perform calculations
+    calculatedByUser(firstArg, secondArg)
+    // Print the results
+    
 }
